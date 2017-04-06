eeb375678238e68724f8bc1b355f7a2b1851bdbd
Khoi Phan <khoiphan21@gmail.com>
2017-04-06 19:55:25 +1000

    fixed up all tests so no more failing tests

--- src/app/stash/stash.component.spec.ts
+++ src/app/stash/stash.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StashComponent } from './stash.component';
+import { SourcesComponent } from '../sources/sources.component';
+import { StashService } from '../stash.service';
+import { AccountService } from '../account.service';
+import { HttpModule } from '@angular/http';
 
 describe('StashComponent', () => {
   let component: StashComponent;
@@ -8,7 +12,17 @@ describe('StashComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StashComponent ]
+      declarations: [
+        StashComponent,
+        SourcesComponent
+      ],
+      providers: [
+        StashService,
+        AccountService
+      ],
+      imports: [
+        HttpModule
+      ]
     })
     .compileComponents();
   }));
