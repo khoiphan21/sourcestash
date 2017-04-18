e91d5af20a4cddc59e685733f07a0b442d64415b
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-04-18 22:42:07 +1000

    background changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,12 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   ngOnInit() {
-    // Particles.init({
-    //   selector: '.background',
-    //   maxParticles: 150,
-    //   connectParticles: true,
-    //   color: '#304269',
-    //   sizeVariations: 2
-    // });
+    Particles.init({
+      selector: '.background',
+      maxParticles: 150,
+      connectParticles: true,
+      color: '#304269',
+      sizeVariations: 2
+    });
   }
 }
