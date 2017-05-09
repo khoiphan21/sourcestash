36aadd807432a2c187a7f5403b2059c24f8d85e0
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-05-09 20:48:37 +1000

    stash page collapsable tab added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // <-- import required BrowserAnimationsModule
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { CollapsibleModule } from 'angular2-collapsible'; // <-- import the module
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -37,12 +40,14 @@ import { FooterComponent } from './footer/footer.component';
     WelcomepageComponent,
     PopupComponent,
     FooterComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapsibleModule,
+    BrowserAnimationsModule
   ],
   providers: [
     AccountService,
