2015fff80adc70c5012de99ee47b1a6c47bf8db5
Khoi Phan <khoiphan21@gmail.com>
2017-05-12 18:51:29 +1000

    fixed up all tests with BETA SERVER

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AccountService } from '../account.service';
+import { GoogleApiService } from '../google-api.service';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +12,15 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [
+        AccountService,
+        GoogleApiService,
+        {provide: Router, useValue: {navigate: jasmine.createSpy('navigate')}}
+      ],
+      imports: [
+        HttpModule
+      ]
     })
     .compileComponents();
   }));
