17694618e797c57b32b8350f98e63f97f39817df
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-05-13 20:42:26 +1000

    change the tab in profile page from component into using ngif and slight style change

--- src/app/profilepage/profilepage.component.ts
+++ src/app/profilepage/profilepage.component.ts
@@ -14,7 +14,11 @@ import { TabsComponent } from '../tabs/tabs.component';
 export class ProfilepageComponent implements OnInit {
   userAccount: Account;
   tabs: TabComponent[] = [];
-  
+  isStashesClicked: boolean = true;
+  isSettingsClicked: boolean = false;
+  isCreateClicked: boolean = false;
+
+
   constructor(private accountService: AccountService) {
   }
 
@@ -22,8 +26,22 @@ export class ProfilepageComponent implements OnInit {
     this.accountService.getUserInformation('user').then(account => {
       this.userAccount = account;
     });
+  }
 
-    
+  clickTab(tabname: string) {
+    if (tabname == 'clickStash') {
+      this.isStashesClicked = true;
+      this.isSettingsClicked = false;
+      this.isCreateClicked = false;
+    } else if (tabname == 'clickSetting') {
+      this.isStashesClicked = false;
+      this.isSettingsClicked = true;
+      this.isCreateClicked = false;
+    } else if (tabname == 'clickCreate') {
+      this.isStashesClicked = false;
+      this.isSettingsClicked = false;
+      this.isCreateClicked = true;
+    }
   }
 
 }
