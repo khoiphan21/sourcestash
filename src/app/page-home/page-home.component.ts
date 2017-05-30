5636ac9d7631fb237e16166c46f5c1aed7a95482
Khoi Phan <khoiphan21@gmail.com>
2017-05-31 01:31:26 +1000

    added member photos

--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -67,6 +67,7 @@ export class PageHomeComponent implements OnInit {
     this.hideAllModals();
   }
   showModal(modalType: string) {
+    console.log('showing modal: ' + modalType)
     this.isModalShown = true;
     this.hideAllModals();
     // Then selectively show the modals
