433c735d27120b61b1b383e9134f4072772abc9a
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-06-27 14:34:56 +1000

    board component interface modification

--- src/app/page-dashboard/page-dashboard.component.ts
+++ src/app/page-dashboard/page-dashboard.component.ts
@@ -10,10 +10,24 @@ export class PageDashboardComponent implements OnInit {
   // Variables to control modal items display
   isModalShown: boolean = false;
   isAddBoardShown: boolean = false;
+  isCreateShown: boolean = true;
+  isFormShown: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  switchPage(name: string){
+    if (name == 'form') {
+      this.isFormShown = true;
+      this.isCreateShown = false;
+    } else if (name == 'create') {
+      this.isCreateShown = true;
+      this.isFormShown = false;
+    }
+  }
+  
 
   /**
    * ALL MODAL FUNCTIONS GO HERE
