132463da82403b279ca516d2abc6f7bf4edf41bf
Jeremy Ng Yung Hau <nicholas5352@gmail.com>
2017-06-27 22:27:50 +1000

    Card service - getCardForBoard, AddNewCard, UpdateCard, DeleteCard functions

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -37,10 +37,10 @@ export class CardService {
     let headers = new Headers({
       'Content-Type': 'applucation/json'
     });
-    let options = new RequestOptions({ headers: headers});
+    let options = new RequestOptions({ headers: headers });
 
     // The deferred promise
-    let deferred = new Deferred<Card[]>(); 
+    let deferred = new Deferred<Card[]>();
 
     /**
      * Make a call to the server to get the array of cards
@@ -61,6 +61,119 @@ export class CardService {
       deferred.reject(error);
     });
 
+    return deferred.promise;
+  }
+
+  /**
+   * Add a new card to the remote database. 
+   * 
+   * @param board_id - the id of the stash this source belongs
+   * @param title - the title of the source
+   * @param x_location - the relative x position of the source
+   * @param y_location - the relative y position of the source
+   * @return return a card object with an id generated
+   */  
+  addNewCard(board_id: string, title: string, x_location: number, y_location: number): Promise<Card>{
+    // Check for null args
+    if(this.checkForNull([board_id, title, x_location, y_location])){
+      return Promise.reject('Null arguments received');
+    }
+
+    // Setting up
+    let deferred = new Deferred<Card>();
+
+    // Calling server
+    this.http.post(
+      SERVER + '/card/new',
+      {
+        card: {
+          board_id: board_id,
+          title: title,
+          x_location: x_location, 
+          y_location: y_location
+        }
+      }
+    ).subscribe(response => {
+    if (response.status != 201){
+      deferred.reject('Failed to create a new card');
+    } else {
+      deferred.resolve(response.json());
+    }
+  }, error => {
+    deferred.reject(error);
+  });
+
+  return deferred.promise;
+}
+
+  /**
+  * @param card - the card which is going to be updated
+  */
+  updateCard(card: Card): Promise<AppResponse> {
+    // Check for mull args
+    if (this.checkForNull([card])){
+      return Promise.reject('Null arguments received');
+    }
+    
+    // Setting up
+    let deferred = new Deferred<AppResponse>();
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({ headers: headers});   
+
+    // Making call to the server
+    this.http.post(
+      SERVER + '/card/update',
+      {
+        card: card
+      },
+      options
+    ).subscribe( response => {
+      if (response.status == 200) {
+        deferred.resolve(new AppResponse(true, response.text()))
+      } else {
+        deferred.reject(new AppResponse(false, response.text()));
+      }
+    }, error => {
+      deferred.reject(new AppResponse(false, error, error));
+    })
+
+    return deferred.promise;
+  }
+
+  /**
+  * @param card_id - the card which is going to be delete
+  */
+  deleteSource(card_id: string): Promise<AppResponse> {
+    // Check for null args
+    if(this.checkForNull([card_id])) {
+      return Promise.reject('Null arguments received');
+    }
+
+    // Setting up
+    let deferred = new Deferred<AppResponse>();
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({ headers: headers});
+
+    // Call the server
+    this.http.post(
+      SERVER + '/source/delete/' + card_id,
+      {
+        card_id: card_id
+      },
+      options
+    ).subscribe(response => {
+      if (response.status == 200) {
+        deferred.resolve(new AppResponse(true, 'Deleted Successfully'));
+      } else {
+        deferred.reject(response.text());
+      }
+    }, error => {
+      deferred.reject(error);
+    })
     return deferred.promise;
   }
 
