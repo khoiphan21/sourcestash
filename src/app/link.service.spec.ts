59b36e30c8d619391024aa7ef01813ecd0c5d2b8
Khoi Phan <khoiphan21@gmail.com>
2017-07-03 21:15:10 +1000

    written basic tests for LinkService

--- src/app/link.service.spec.ts
+++ src/app/link.service.spec.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { AccountService } from './account.service';
 import { GoogleApiService } from './google-api.service';
 import { Router } from '@angular/router';
+import { Link } from './classes/link';
 
 describe('LinkService', () => {
   beforeEach(() => {
@@ -27,19 +28,58 @@ describe('LinkService', () => {
 
   it('should create a link and then delete it', done => {
     inject([LinkService], (service: LinkService) => {
-
+      let title = 'Link Title';
+      let stackingOrder = 0
+      service.createLink(title, stackingOrder, 0, 0).then((link: Link) => {
+        expect(link).toBeTruthy();
+        expect(link.title).toBe(title);
+        return service.deleteLink(link.link_id);
+      }).then(response => {
+        expect(response.success).toBeTruthy();
+        done();
+      }).catch(error => {
+        fail('Error occurred while trying to create and delete a link');
+        done();
+      })
     })();
   })
 
   it('should update a link successfully', done => {
     inject([LinkService], (service: LinkService) => {
+      let title = 'Link Title';
+      let changedTitle = 'Changed Title';
+      let stackingOrder = 0
+      service.createLink(title, stackingOrder, 0, 0).then((link: Link) => {
+        expect(link).toBeTruthy();
+        expect(link.title).toBe(title);
+        
+        // Now change the link's title
+        link.title = changedTitle;
 
+        return service.updateLink(link);
+      }).then((link: Link) => {
+        expect(link.title).toBe(changedTitle);
+        // now delete the link
+        return service.deleteLink(link.link_id);
+      }).then(() => {
+        done();
+      }).catch(error => {
+        fail('Error trying to update a link');
+        done();
+      });
     })();
   })
 
   it('should retrieve all links for a card successfully', done => {
     inject([LinkService], (service: LinkService) => {
-
+      let card_id = 'abcd';
+      service.getLinkForCard(card_id).then((links: Link[]) => {
+        expect(links).toBeTruthy();
+        done();
+      }).catch(error => {
+        fail('Error trying to retrieve links for a card');
+        done();
+      })
     })();
   })
 });
--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Deferred } from './classes/deferred';
+import { Link } from './classes/link';
+import { AppResponse } from './classes/response';
 
 @Injectable()
 export class LinkService {
@@ -10,7 +13,28 @@ export class LinkService {
     stackingOrder: number, 
     xPosition: number, 
     yPosition: number
-  ) {
+  ): Promise<Link> {
+    let deferred = new Deferred<Link>();
+
+    return deferred.promise;
+  }
+
+  updateLink(link: Link): Promise<Link> {
+    let deferred = new Deferred<Link>();
+
+    return deferred.promise;
+  }
+
+  getLinkForCard(card_id: string): Promise<Link[]> {
+    let deferred = new Deferred<Link[]>();
+
+    return deferred.promise;
+  }
+
+  deleteLink(link_id: string): Promise<AppResponse> {
+    let deferred = new Deferred<AppResponse>();
+
+    return deferred.promise;
 
   }
 
